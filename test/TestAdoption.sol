901ec0b7dde9118c7ea729f6f58bd9541d3d5adc
Waqas Ahmed - Sharaf DG <waqas.ahmed@sharafdg.com>
2019-01-24 08:02:35 +0400

    Added 2 more solidity tests

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -20,4 +20,19 @@ contract TestAdoption {
 
         Assert.equal(returnedId, expectedPetId, "Adoption of the expected pet should match what is returned.");
     }
+
+    // Testing retrieval of a single pet's owner
+    function testGetAdopterAddressByPetId() public {
+        address adopter = adoption.adopters(expectedPetId);
+
+        Assert.equal(adopter, expectedAdopter, "Owner of the expected pet should be this contract");
+    }  
+
+    // Testing retrieval of all pet owners
+    function testGetAdopterAddressByPetIdInArray() public {
+        // Store adopters in memory rather than contract's storage
+        address[16] memory adopters = adoption.getAdopters();
+
+        Assert.equal(adopters[expectedPetId], expectedAdopter, "Owner of the expected pet should be this contract");
+    }      
 }
